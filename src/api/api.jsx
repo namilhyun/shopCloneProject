ad68bd72e6cd5736d4dd33f5451e823efc12051b
namIH <dlfgus0217@naver.com>
2024-05-07 11:44:07 +0900

    API 숨기기

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,11 +1,7 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBoQ6HAsR0xadK2EF2gQqX4zXEYKmjxEYU",
-    authDomain: "ih-shop.firebaseapp.com",
-    projectId: "ih-shop",
-    storageBucket: "ih-shop.appspot.com",
-    messagingSenderId: "535331966135",
-    appId: "1:535331966135:web:5033c39e9979c456889132",
-    measurementId: "G-Z48W20QWLN"
+    apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: NEXT_PUBLIC_FIREBASE_API_AUTH_DOMAIN,
+    projectId: NEXT_PUBLIC_FIREBASE_API_AUTH_PROJECT_ID
   };
