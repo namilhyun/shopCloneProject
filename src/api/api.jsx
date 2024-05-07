f0dce2c549b1ab85983e4810bd441d6c148b6542
namIH <dlfgus0217@naver.com>
2024-05-07 11:37:49 +0900

    api키 업로드

---
+++ src/api/api.jsx
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBoQ6HAsR0xadK2EF2gQqX4zXEYKmjxEYU",
+    authDomain: "ih-shop.firebaseapp.com",
+    projectId: "ih-shop",
+    storageBucket: "ih-shop.appspot.com",
+    messagingSenderId: "535331966135",
+    appId: "1:535331966135:web:5033c39e9979c456889132",
+    measurementId: "G-Z48W20QWLN"
+  };
